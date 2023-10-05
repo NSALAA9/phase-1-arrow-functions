f4f84d69a76a8d86ae24dfed9932491703924525
NSALAA9 <alaansairat99@gmail.com>
2023-10-05 07:42:40 +0000

    Completed

---
+++ index.js
@@ -0,0 +1,12 @@
+
+function divide() {
+    return 2000 / 100;
+  }
+  
+  // Arrow function called square
+  const square = (x) => x * x;
+  
+  // Arrow function called add
+  const add = (a, b) => a + b;
+  
+  
